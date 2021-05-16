1a28059dfc7a3dcf1a04657fce8ac2714cc13671
David <juan.alvarez@utp.edu.co>
2021-05-16 17:37:45 -0500

    version 1.0

---
+++ proyecto1/proyecto1/views.py
@@ -0,0 +1,48 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import Template, Context
+from django.template.loader import get_template
+import datetime 
+import pymongo
+
+
+def saludo(request):
+    hora=datetime.datetime.now()
+    return render(request,'paginaweb.html',{"hora":hora}) 
+
+
+def busquedaLibro(request):
+     return render(request,'paginaweb.html') 
+
+
+
+
+def buscar(request):
+    cliente = pymongo.MongoClient("mongodb+srv://admin:33sqQMSJRct-Erz@cluster0.nfxzs.mongodb.net/Libreria?retryWrites=true&w=majority")
+    db = cliente.Libreria
+
+    db=cliente['Libreria']
+
+    mensaje=request.GET["buscarBD"]
+
+    libro = db['libro']
+    buscar=libro.find_one({'titulo':mensaje})
+    buscar=buscar['autor']
+    cliente.close()
+
+    return HttpResponse(buscar) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
